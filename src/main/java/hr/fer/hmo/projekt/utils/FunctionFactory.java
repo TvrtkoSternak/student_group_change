79b8f4d92f6b53af74d3c1d898ad36edd1de64cc
tomislavantebrncic <tomislav-ante.brncic@fer.hr>
2019-01-10 22:33:17 +0100

    Izmjene u FunctionFactory
    pointsA, pointsB, pointsC

--- src/main/java/hr/fer/hmo/projekt/utils/FunctionFactory.java
+++ src/main/java/hr/fer/hmo/projekt/utils/FunctionFactory.java
@@ -4,26 +4,42 @@ import hr.fer.zemris.optjava.dz3.IFunction;
 
 public class FunctionFactory {
 
-	public static IFunction getFunction(long[] students, long[] requests, long[] overlaps, long[] limits, final HardConstraints hardConstraints) {
+	public static IFunction getFunction(long[] students, long[] requests, long[] overlaps, long[] limits, long[] awardActivity, long awardStudents, long minMaxPenalty, final HardConstraints hardConstraints) {
+		HashMap<Long, Long> studentActivityCount = new HashMap<>();
+		for (int i = 0; i < students.length; i += 5) {
+			studentActivityCount.merge(students[i*5], 1, Integer::sum);
+		}
 		
 		return new IFunction() {
 			
-			
+			private HashMap<Long, Long> studentActivityCountFull = studentActivityCount;
+			private HashMap<Long, Long> studentActivityCountSolution = new HashMap<>();
+		
 			public double valueAt(long[] solution) {
-				if (hardConstraints.fulfilled(solution)) {
-					return pointsA(solution) + pointsB(solution) + pointsC(solution) - pointsD(solution) - pointsE(solution);
-				}
-				return -1;
+				return pointsA(solution, students) + pointsB(solution) + pointsC(solution) - pointsD(solution) - pointsE(solution);
 			}
 			
-			private long pointsA(long[] solution) {
+			public double fitness(long[] solution) {
+				return pointsA(solution, students) + pointsB(solution) + pointsC(solution) - pointsD(solution) - pointsE(solution) - hardConstraints.penalty(solution);
+			}
+			
+			private long pointsA(long[] solution, long[] students) {
+				studentActivityCountSolution.clear();
+				long sum = 0;
+				int j = 0;
 				for (int i = 0; i < solution.length; i += 6) {
-					if ()
+					if (hardConstraints.hasRequest() && hardConstraints.noOverlaps() && solution[3] != solution[4]) {
+						sum += students[j*5 + 2];
+						studentActivityCountSolution.merge(solution[i], 1, Integer::sum);
+					}
+					j++;
 				}
 			}
 			
 			private long pointsB(long[] solution) {
-				
+				int size = awardActivity.length;
+				studentActivityCountSolution.entrySet()
+					.map((k,v) -> studentActivityCountFull()
 			}
 			
 			private long pointsC(long[] solution) {
